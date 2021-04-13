8192765119e281a4df58e9e569b0c0c54b03468b
reandlyarahdian <52603915+reandlyarahdian@users.noreply.github.com>
2021-04-14 02:30:49 +0700

    Update Movement

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private CharacterController character;
-    [SerializeField] private Vector3 move;
+    [SerializeField] private Vector3 move = Vector3.forward;
     [SerializeField] private float speed;
     [SerializeField] private float jump;
-    [SerializeField] public MoveNRotate trigger;
+    private float gravity = 9.81f;
     Vector3 rotate;
     public static Movement M;
 
@@ -20,42 +20,33 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && character.isGrounded)
         {
-            if (trigger == MoveNRotate.left)
-            {
-                move = Vector3.left;
-                rotate = new Vector3(0, 270, 0);
-            }
-            else if (trigger == MoveNRotate.right)
-            {
-                move = Vector3.right;
-                rotate = new Vector3(0, 90, 0); ;
-            }
-            else if (trigger == MoveNRotate.forward)
-            {
-                move = Vector3.forward;
-                rotate = new Vector3(0, 0, 0); ;
-            }
-            else if (trigger == MoveNRotate.backward)
-            {
-                move = Vector3.back;
-                rotate = new Vector3(0, 180, 0); ;
-            }
+            StartCoroutine(jumping());
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            move = Vector3.left;
+            rotate = new Vector3(0, -90, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            move = Vector3.forward;
+            rotate = new Vector3(0, 0, 0);
         }
     }
 
     private void FixedUpdate()
     {
         transform.rotation = Quaternion.Euler(rotate);
+        move.y -= gravity * Time.deltaTime;
         character.Move(move * speed);
     }
-}
 
-public enum MoveNRotate
-{
-    left,
-    right,
-    forward,
-    backward
+    IEnumerator jumping()
+    {
+        move = Vector3.up * jump;
+        yield return new WaitForSeconds(0.5f);
+        move = Vector3.forward;
+    }
 }

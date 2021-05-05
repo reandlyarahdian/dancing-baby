604f907843b26eeb712c23bae54556db854a8225
reandlyarahdian <52603915+reandlyarahdian@users.noreply.github.com>
2021-05-05 22:37:26 +0700

    #3

---
+++ Assets/Script/CamMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    Vector3 offset;
+    [SerializeField]
+    Transform target;
+
+    void Start()
+    {
+        this.offset = transform.position - target.position;
+    }
+
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, this.target.position + offset, 8f * Time.deltaTime);
+    }
+}
